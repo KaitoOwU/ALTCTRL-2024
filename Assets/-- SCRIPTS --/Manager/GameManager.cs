fa046f44d0ffd3fe856cd617535c1c7cb81ce7cc
Kaito <advestankv@gmail.com>
2024-10-21 10:32:03 +0200

    LE TOOL EST VRAIMENT FINI

--- Assets/-- SCRIPTS --/Manager/GameManager.cs
+++ Assets/-- SCRIPTS --/Manager/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -23,11 +24,34 @@ public class GameManager : MonoBehaviour
     [SerializeField] private AudioSource _audioSource;
     private void Update()
     {
-        _valueOfMusicYippie.text = _soundtracker.curves[0].curve.Evaluate(_audioSource.time).ToString();
+        float f = _soundtracker.curves[0].curve.Evaluate(_audioSource.time);
+        _valueOfMusicYippie.text = f.ToString();
+        
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Debug.Log(_valueOfMusicYippie.text);
+            string debug = _valueOfMusicYippie.text;
+            switch (f)
+            {
+                case < 0.25f:
+                    debug += $"<b><color=#{new Color(0.7f, 0.7f, 0.7f).ToHexString()}> MISSED</color></b>";
+                    break;
+                case < 0.5f:
+                    debug += $"<b><color=#{new Color(0.7f, 0.2f, 0.2f).ToHexString()}> BAD</color></b>";
+                    break;
+                case < 0.75f:
+                    debug += $"<b><color=#{Color.blue.ToHexString()}> OK</color></b>";
+                    break;
+                case < 0.9f:
+                    debug += $"<b><color=#{Color.yellow.ToHexString()}> NICE</color></b>";
+                    break;
+                case >= 0.9f:
+                    debug += $"<b><color=#{Color.green.ToHexString()}> PERFECT</color></b>";
+                    break;
+            }
+            
+            Debug.Log(debug);
         }
     }
 
