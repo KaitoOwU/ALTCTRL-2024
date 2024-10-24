49bbd1a512352c5691cc97162af81e3c863545eb
Nic0InTheRadio <112950466+Nic0InTheRadio@users.noreply.github.com>
2024-10-24 17:28:44 +0200

    Revert "Merge branch 'Lucas' into NicoC(lemeilleur)"
    This reverts commit c01523387a83b2179b4e786586acffb884095ffb, reversing
    changes made to a0c78e1299da7254c59bdb162ad7384aa3dbbd32.

--- Assets/-- SCRIPTS --/Manager/GameManager.cs
+++ Assets/-- SCRIPTS --/Manager/GameManager.cs
@@ -19,32 +19,59 @@ public class GameManager : MonoBehaviour
     [field:SerializeField] public float PropsSpeed { get; private set; }
     [field:SerializeField] public Transform SpawnPoint { get; private set; }
 
-    public Action onBeat;
+    [SerializeField] private TMP_Text _tmp, _scoring;
+    [SerializeField] private PlayableDirector _directorMelody, _directorDrums;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private GameData _gameData;
+
+    public Action onRealBeat, onPlayerBeat;
     [SerializeField] private SignalReceiver _signals;
     private float _beatStatus = 0f;
     
-    /*public EInputPrecision InputPrecision
+    public EInputPrecision InputPrecision
     {
         get
         {
-            if (InputValue >= GameData.perfectTolerance)
+            if (_beatStatus >= _gameData.perfectTolerance)
                 return EInputPrecision.PERFECT;
-            if (InputValue >= GameData.niceTolerance)
+            if (_beatStatus >= _gameData.niceTolerance)
                 return EInputPrecision.NICE;
-            if (InputValue >= GameData.okTolerance)
-                return EInputPrecision.OK;
-            return EInputPrecision.MISSED;
+            return _beatStatus >= _gameData.okTolerance ? EInputPrecision.OK : EInputPrecision.MISSED;
         }
-    }*/
+    }
     
-    private float _score;
+    private float _score = 0f;
+    [SerializeField] private float _timeAfterBeatValid;
 
     private int _currentCurve = 0;
     private void Update()
     {
+        _beatStatus = Mathf.Clamp01(_beatStatus - Time.deltaTime / _timeAfterBeatValid);
+        _audioSource.volume = Mathf.Clamp(_audioSource.volume - Time.deltaTime / _timeAfterBeatValid * 5f, 0.3f, 1f);
+        _tmp.text = Math.Round(_beatStatus).ToString(CultureInfo.CurrentCulture);
         if (CustomMidi.GetKeyDown(CustomMidi.MidiKey.NOTE_KEY) || Input.GetKeyDown(KeyCode.Space))
         {
-            
+            OnPlayerBeat();
+            switch (InputPrecision)
+            {
+                case EInputPrecision.PERFECT:
+                    _scoring.text = $"<color=#{Color.green.ToHexString()}>PERFECT</color><br>" + _scoring.text;
+                    _audioSource.volume = 1f;
+                    break;
+                case EInputPrecision.NICE:
+                    _scoring.text = $"<color=#{Color.cyan.ToHexString()}>NICE</color><br>" + _scoring.text;
+                    _audioSource.volume = .9f;
+                    break;
+                case EInputPrecision.OK:
+                    _scoring.text = $"<color=#{Color.yellow.ToHexString()}>OK</color><br>" + _scoring.text;
+                    _audioSource.volume = .8f;
+                    break;
+                case EInputPrecision.MISSED:
+                    _scoring.text = $"<color=#{Color.red.ToHexString()}>MISSED</color><br>" + _scoring.text;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 
@@ -54,12 +81,34 @@ public class GameManager : MonoBehaviour
             Destroy(this.gameObject);
 
         Instance = this;
+        
+        StartCoroutine(StartBeat());
     }
 
-    public void OnBeat()
+    private IEnumerator StartBeat()
     {
-        onBeat?.Invoke();
-        Debug.Log("OnBeat");
+        _directorMelody.Play();
+        _directorDrums.Play();
+        yield return new WaitForSecondsRealtime(1f);
+        _directorMelody.Pause();
+        _directorDrums.Pause();
+        _directorMelody.time = 0;
+        _directorDrums.time = 0;
+        yield return new WaitForSecondsRealtime(1f);
+        _directorMelody.Play();
+        _directorDrums.Play();
+    }
+
+    public void OnRealBeat()
+    {
+        onRealBeat?.Invoke();
+
+        _beatStatus = 1f;
+    }
+
+    public void OnPlayerBeat()
+    {
+        onPlayerBeat?.Invoke();
     }
 
     #if UNITY_EDITOR
